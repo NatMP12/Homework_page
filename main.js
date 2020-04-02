1e59e0f14852221105dd47b0859d7ba86ecd5155
unknown <marco.boedecker896@std.uni.edu.ni>
2020-04-01 21:18:03 -0600

    subir archivos para iniciar

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('.up').hide(); 
+    
+    $(window).scroll(function(){
+       if($(this).scrollTop() > 100 ){
+           $('.up').fadeIn();
+       } else{
+           $('.up').fadeOut();
+       }
+    });
+    
+    $('.up').click(function(){
+       $('body, html').animate({
+          scrollTop: 0 
+       }); 
+    });
+    
+   $(".up").on('click', function (e) { 
+       e.preventDefault();
+     $("body,html").animate({
+    scrollTop: 0
+          },700);
+        return false; 
+});  
+       
+       
+});
